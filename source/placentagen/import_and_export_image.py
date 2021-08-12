89c25b6d3dcb7ab4b441ad41bba392cd27d51638
Alys Clark <acla148@bn405952.uoa.auckland.ac.nz>
2021-08-12 12:38:40 +1200

    basic analyse image

---
+++ source/placentagen/import_and_export_image.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import numpy as np
+import SimpleITK as sitk
+
+"""
+.. module:: import_and_export_image
+  :synopsis: Contains code to read and write different image formats, primarily based on simpleITK.
+
+:synopsis:Contains code to read and write different image formats
+
+
+"""
+
+def read_nifti(path):
+    """
+        :Function name: **read_nifti**
+
+        Uses SimpleITK to read in a nifti file.
+
+        :inputs:
+           - path: Text object, path to file.
+
+        return:
+           - img: A
+    """
+    img = sitk.ReadImage(path)
+    return img
